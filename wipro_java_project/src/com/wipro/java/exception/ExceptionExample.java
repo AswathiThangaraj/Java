a5ce424568f3c4f5d7be50c92418234178625bc7
Aswathi2307 <84922043+Aswathi2307@users.noreply.github.com>
2025-02-04 15:17:44 +0530

    Day 3

--- wipro_java_project/src/com/wipro/java/exception/ExceptionExample.java
+++ wipro_java_project/src/com/wipro/java/exception/ExceptionExample.java
@@ -7,7 +7,6 @@ package com.wipro.java.exception;
  * 
  */
 public class ExceptionExample {
-	static int num = 5;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num=9;

a3fd77855a685525e17f036be8cfe3444fd21e45
Aswathi2307 <84922043+Aswathi2307@users.noreply.github.com>
2025-02-07 12:36:36 +0530

    Day 6

--- wipro_java_project/src/com/wipro/java/junit/CalculatorTest.java
+++ wipro_java_project/src/com/wipro/java/junit/CalculatorTest.java
@@ -9,25 +9,25 @@ public class CalculatorTest {
 	
 	@Test
 	public void testAdd() {
-		int result = calculator.add(3, 2);
+		int result = calculator.add(3, 2); //add
 		assertEquals(5, result); //to validate the expected results
 	}
 	
 	@Test
 	public void testSubtract() {
-		int result = calculator.subtract(5, 3);
-		assertEquals(2, result);
+		int result = calculator.subtract(5, 3); //subtract
+		assertEquals(2, result); //to validate the expected results
 	}
 	
 	@Test
 	public void testAddWithNegativeNumbers() {
-		int result = calculator.add(-3, -2);
-		assertEquals(-5, result);
+		int result = calculator.add(-3, -2); //add
+		assertEquals(-5, result); //to validate the expected results
 	}
 	
 	@Test
 	public void testSubtractWithNegativeNumbers() {
-		int result = calculator.subtract(-3, -2);
-		assertEquals(-1, result);
+		int result = calculator.subtract(-3, -2); //subtract
+		assertEquals(-1, result); //to validate the expected results
 	}
 }
